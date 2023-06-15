e56fdbf6b7215c4cbe65cdb5ca6c0aac14ed1a04
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-06-15 10:04:53 -0700

    WIP

--- deploy/Program.cs
+++ deploy/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using Pulumi;
+using Pulumi.Azure.Compute;
+using Pulumi.Docker;
 using Azure = Pulumi.Azure;
+using Image = Pulumi.Docker.Image;
+using ImageArgs = Pulumi.Docker.ImageArgs;
 
 return await Deployment.RunAsync(() =>
 {
@@ -14,12 +18,32 @@ return await Deployment.RunAsync(() =>
         ResourceGroupName = example.Name,
         Location = example.Location,
         Sku = "Basic",
-        AdminEnabled = false
+        AdminEnabled = true, // TODO: Find a way to do this via AAD / MSI
+    });
+
+    var acrCredentials = new ImageRegistry
+    {
+        Server = acr.LoginServer,
+        Username = acr.AdminUsername,
+        Password = acr.AdminPassword,
+    };
+
+    var image = new Image("docpad", new ImageArgs
+    {
+        Build = new DockerBuild
+        {
+            Context = "..",
+            Dockerfile = "../build/Dockerfile",
+        },
+        ImageName = Output.Format($"{acr.LoginServer}/docpad:latest"),
+        Registry = acrCredentials,
     });
 
     return new Dictionary<string, object?>
     {
         ["ResourceName"] = acr.GetResourceName(),
         ["Name"] = acr.Name,
+        ["FullImageName"] = image.ImageName,
+        ["ImageHash"] = image.ImageName.Apply(x => x.Split(":")[1]),
     };
 });
